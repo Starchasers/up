523c3d73e324df93dbb6be6f7b249c1a2d347b2a
Kuboczoch <jakub.pokorski@protonmail.com>
2020-04-15 21:47:47 +0200

    Gatsby init

---
+++ gatsby-app/src/components/layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Layout = ({ children }) => (
+  <>
+    <main>
+      {children}
+    </main>
+  </>
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
