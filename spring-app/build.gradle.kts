e85aa358d1af90b05ca6ae3785b2a87bbf50b535
Hubert Balcerzak <hubertbalc@gmail.com>
2020-04-15 13:23:36 +0200

    fix tests

--- spring-app/build.gradle.kts
+++ spring-app/build.gradle.kts
@@ -33,6 +33,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("mysql:mysql-connector-java")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+    testImplementation("com.h2database:h2:1.4.200")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
