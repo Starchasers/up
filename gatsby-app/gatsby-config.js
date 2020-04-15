5f8d5bbf5131c4725f7418ca16e94e33caa9f7f9
Kuboczoch <jakub.pokorski@protonmail.com>
2020-04-15 23:12:54 +0200

    Favicon and global gatsby-config.js

--- gatsby-app/gatsby-config.js
+++ gatsby-app/gatsby-config.js
@@ -1,22 +1,62 @@
 module.exports = {
   siteMetadata: {
-    title: `UP Website`,
-    description: `Up website for storing files`,
-    author: `@Kuboczoch`,
+    title: 'UP Website',
+    description: 'Up website for storing files',
+    author: '@Kuboczoch',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `up-website`,
-        short_name: `up`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        name: 'images',
+        path: `${__dirname}/src/assets/images`,
       },
-    }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'up-website',
+        short_name: 'up',
+        start_url: '/',
+        background_color: '#1A1B1C',
+        theme_color: '#2c369a',
+        icon: 'src/assets/images/favicon.png',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/assets/images/favicon.png',
+        appDescription: 'Private file hosting',
+        developerName: 'Kuboczoch',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-styled-components',
+      options: {
+        displayName: false,
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 }
