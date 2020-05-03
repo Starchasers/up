a02d3f8f643702e679eaa1a8b39dc39e1876d52d
Kuboczoch <jakub.pokorski@protonmail.com>
2020-05-03 23:34:38 +0200

    Refactor constants

--- gatsby-app/src/components/AfterPageBox.js
+++ gatsby-app/src/components/AfterPageBox.js
@@ -6,30 +6,30 @@ import { connect } from 'react-redux'
 import { setPage, setResponse } from '../redux/actions'
 import { PAGE_ID } from '../redux/constants'
 
-const AfterPageBox = ({ response, dispatch }) => (
-  <AfterUploadBox>
-    <AfterUploadBox.TextBox>
-      <AfterUploadBox.Text
-        target='_blank'
-        href={`${process.env.GATSBY_API_URL
-          ? process.env.GATSBY_API_URL
-          : ''}/u/` + response.data.key}
-      >
-        {`${process.env.GATSBY_API_URL
-          ? process.env.GATSBY_API_URL
-          : window.location.origin}/u/` + response.data.key}
-      </AfterUploadBox.Text>
-    </AfterUploadBox.TextBox>
-    <AfterUploadBox.Back
-      onClick={() => {
-        dispatch(setResponse({ received: false, data: {} }))
-        dispatch(setPage({ pageId: PAGE_ID.MAIN_PAGE }))
-      }}>
-      <AfterUploadBox.Icon icon={faAngleLeft}/>
-      Go back
-    </AfterUploadBox.Back>
-  </AfterUploadBox>
-)
+const AfterPageBox = ({ response, dispatch }) => {
+  const hrefLink = (process.env.GATSBY_API_URL ? process.env.GATSBY_API_URL : '') + '/u/'
+  const displayLink = (process.env.GATSBY_API_URL ? process.env.GATSBY_API_URL : window.location.origin) + '/u/'
+  return (
+    <AfterUploadBox>
+      <AfterUploadBox.TextBox>
+        <AfterUploadBox.Text
+          target='_blank'
+          href={hrefLink + response.data.key}
+        >
+          {displayLink + response.data.key}
+        </AfterUploadBox.Text>
+      </AfterUploadBox.TextBox>
+      <AfterUploadBox.Back
+        onClick={() => {
+          dispatch(setResponse({ received: false, data: {} }))
+          dispatch(setPage({ pageId: PAGE_ID.MAIN_PAGE }))
+        }}>
+        <AfterUploadBox.Icon icon={faAngleLeft}/>
+        Go back
+      </AfterUploadBox.Back>
+    </AfterUploadBox>
+  )
+}
 
 const mapStateToProps = store => {
   const response = getResponseState(store)
