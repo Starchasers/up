a36ab03ed8cc15ee7c7da4182ff50f653f78d73e
Hubert Balcerzak <hubertbalc@gmail.com>
2020-07-27 02:49:18 +0200

    check failing test

--- spring-app/src/test/kotlin/pl/starchasers/up/UpApplicationTests.kt
+++ spring-app/src/test/kotlin/pl/starchasers/up/UpApplicationTests.kt
@@ -1,5 +1,6 @@
 package pl.starchasers.up
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +9,7 @@ class UpApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		Assertions.assertEquals("qwe", "asd")
 	}
 
 }
