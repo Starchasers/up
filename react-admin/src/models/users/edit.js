1308a2035d9fc52675f785dbf183ee2a91573711
Kuboczoch <jakub.pokorski@protonmail.com>
2020-10-10 01:09:21 +0200

    Material UI style
    Removed yarn.lock, because we are not using it anyway

--- react-admin/src/models/users/edit.js
+++ react-admin/src/models/users/edit.js
@@ -10,9 +10,17 @@ import {
   SaveButton,
   DeleteButton
 } from 'react-admin'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+})
 
 const CustomToolbar = (props) => (
-  <Toolbar {...props} style={{ display: 'flex', justifyContent: 'space-between' }}>
+  <Toolbar {...props} classes={useStyles()}>
     <SaveButton undoable={false} />
     <DeleteButton undoable={true} />
   </Toolbar>
