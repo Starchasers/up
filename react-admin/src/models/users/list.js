3f3c6b535993fa997d8ac3fcd9f91217f67fc07b
Kuboczoch <jakub.pokorski@protonmail.com>
2020-10-22 21:16:40 +0200

    Changed list perPage pagination

--- react-admin/src/models/users/list.js
+++ react-admin/src/models/users/list.js
@@ -3,13 +3,20 @@ import {
   List,
   Datagrid,
   TextField,
-  EditButton
+  EditButton,
+  Pagination
 } from 'react-admin'
+
+const UserPagination = (props) => (
+  <Pagination rowsPerPageOptions={[10, 15, 25, 50, 100]} {...props} />
+)
 
 export default (props) => (
   <List
     {...props}
     bulkActionButtons={false}
+    perPage={15}
+    pagination={<UserPagination/>}
   >
     <Datagrid>
       <TextField
