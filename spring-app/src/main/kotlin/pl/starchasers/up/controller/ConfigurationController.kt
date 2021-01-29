ef3156210f9afcc2f72953ca3c1184fb7a88750d
Hubert Balcerzak <hubertbalc@gmail.com>
2021-01-29 23:58:01 +0100

    release workarounds

--- spring-app/src/main/kotlin/pl/starchasers/up/controller/ConfigurationController.kt
+++ spring-app/src/main/kotlin/pl/starchasers/up/controller/ConfigurationController.kt
@@ -1,5 +1,6 @@
 package pl.starchasers.up.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import pl.starchasers.up.data.dto.configuration.UserConfigurationDTO
@@ -9,9 +10,12 @@ import java.security.Principal
 
 @RestController
 class ConfigurationController(
-        private val configurationService: ConfigurationService,
-        private val userService: UserService
+    private val configurationService: ConfigurationService,
+    private val userService: UserService
 ) {
+
+    @Value("\${up.max-file-size}")
+    private val maxFileSize: Long = 0
 
     /**
      * Returns configuration for anonymous or logged in user, if Authorization header is provided
@@ -20,14 +24,25 @@ class ConfigurationController(
     fun getConfiguration(principal: Principal?): UserConfigurationDTO {
         val user = userService.fromPrincipal(principal)
 
+        //TODO 1.1 release workaround:
+        //TODO go back to user configuration
+
+//        return UserConfigurationDTO(
+//                user?.maxTemporaryFileSize?.value ?: configurationService.getAnonymousMaxFileSize().value,
+//                user?.maxFileLifetime?.value ?: configurationService.getAnonymousMaxFileLifetime().value,
+//                user?.defaultFileLifetime?.value ?: configurationService.getAnonymousDefaultFileLifetime().value,
+//                if (user != null) user.maxFileLifetime.value == 0L else configurationService.getAnonymousDefaultFileLifetime().value == 0L,
+//                user?.maxPermanentFileSize?.value ?: if (configurationService.getAnonymousMaxFileLifetime().value == 0L)
+//                    configurationService.getAnonymousMaxFileSize().value
+//                else 0
+//        )
+        val oneDay: Long = 24 * 60 * 60 * 1000
         return UserConfigurationDTO(
-                user?.maxTemporaryFileSize?.value ?: configurationService.getAnonymousMaxFileSize().value,
-                user?.maxFileLifetime?.value ?: configurationService.getAnonymousMaxFileLifetime().value,
-                user?.defaultFileLifetime?.value ?: configurationService.getAnonymousDefaultFileLifetime().value,
-                if (user != null) user.maxFileLifetime.value == 0L else configurationService.getAnonymousDefaultFileLifetime().value == 0L,
-                user?.maxPermanentFileSize?.value ?: if (configurationService.getAnonymousMaxFileLifetime().value == 0L)
-                    configurationService.getAnonymousMaxFileSize().value
-                else 0
+            maxFileSize,
+            oneDay,
+            oneDay,
+            false,
+            0
         )
     }
 }
