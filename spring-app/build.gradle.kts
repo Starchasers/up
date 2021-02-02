b3711c6dd4c3628e492b01974b5cd692970c5f22
Hubert Balcerzak <hubertbalc@gmail.com>
2021-02-02 01:40:32 +0100

    add flyway, update dependencies

--- spring-app/build.gradle.kts
+++ spring-app/build.gradle.kts
@@ -1,14 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.2.6.RELEASE"
+    id("org.springframework.boot") version "2.4.2"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("org.asciidoctor.jvm.convert") version "3.1.0"
     id("org.jetbrains.dokka") version "0.9.18"
+    id("org.flywaydb.flyway") version "7.5.2"
 
-    kotlin("jvm") version "1.3.71"
-    kotlin("plugin.spring") version "1.3.71"
-    kotlin("plugin.jpa") version "1.3.71"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.spring") version "1.4.21"
+    kotlin("plugin.jpa") version "1.4.21"
 }
 
 group = "pl.starchasers"
@@ -24,11 +25,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("commons-fileupload:commons-fileupload:1.3.3")
     implementation("io.jsonwebtoken:jjwt:0.9.1")
+    implementation("org.flywaydb:flyway-core:7.5.2")
     runtimeOnly("com.h2database:h2:1.4.200")
     implementation("com.ibm.icu:icu4j:67.1")
     runtimeOnly("com.h2database:h2:1.4.200")
@@ -45,14 +48,13 @@ dependencies {
     testImplementation("capital.scalable:spring-auto-restdocs-core:2.0.8")
 
     testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
-//    "jsondoclet"("capital.scalable:spring-auto-restdocs-json-doclet:2.0.7")//was jsondoclet()
     testImplementation("capital.scalable:spring-auto-restdocs-json-doclet-jdk9:2.0.8")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjsr305=strict", "-Xemit-jvm-type-annotations")
+        jvmTarget = "11"
     }
 }
 
