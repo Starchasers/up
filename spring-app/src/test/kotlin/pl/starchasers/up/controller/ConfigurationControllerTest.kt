e4eabf1db369989c540838ccc802b25aa9dd55da
Hubert Balcerzak <hubertbalc@gmail.com>
2021-02-03 03:03:34 +0100

    Feature/enable configuration (#187)
    * add flyway, update dependencies
    
    * better test setup
    
    * enable configuration
    
    * Feature/flyway (#186)
    
    * add flyway, update dependencies
    
    * better test setup
    
    * fix transaction
    
    * fix typo
    
    * better constraint names
    
    * fix naming
    
    * Feature/flyway (#186)
    
    * add flyway, update dependencies
    
    * better test setup
    
    * fix transaction
    
    * fix typo
    
    * better constraint names
    
    * fix naming
    
    * add flyway, update dependencies
    
    * better test setup
    
    * Update spring-app/src/test/kotlin/pl/starchasers/up/controller/ConfigurationControllerTest.kt
    
    Co-authored-by: KocproZ <kocproz@protonmail.com>
    
    * Update spring-app/src/main/kotlin/pl/starchasers/up/controller/ConfigurationController.kt
    
    Co-authored-by: KocproZ <kocproz@protonmail.com>
    
    Co-authored-by: KocproZ <kocproz@protonmail.com>

--- spring-app/src/test/kotlin/pl/starchasers/up/controller/ConfigurationControllerTest.kt
+++ spring-app/src/test/kotlin/pl/starchasers/up/controller/ConfigurationControllerTest.kt
@@ -1,17 +1,23 @@
 package pl.starchasers.up.controller
 
 import no.skatteetaten.aurora.mockmvc.extensions.Path
+import no.skatteetaten.aurora.mockmvc.extensions.authorization
+import no.skatteetaten.aurora.mockmvc.extensions.get
+import no.skatteetaten.aurora.mockmvc.extensions.responseJsonPath
 import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import pl.starchasers.up.JpaTestBase
-import pl.starchasers.up.MockMvcTestBase
-import pl.starchasers.up.OrderTests
+import org.springframework.http.HttpHeaders
+import pl.starchasers.up.*
+import pl.starchasers.up.data.model.ConfigurationKey
+import pl.starchasers.up.data.value.Username
+
 import pl.starchasers.up.service.JwtTokenService
 import pl.starchasers.up.service.UserService
 
 internal class ConfigurationControllerTest(
-        @Autowired private val userService: UserService,
-        @Autowired private val jwtTokenService: JwtTokenService
+    @Autowired private val userService: UserService,
+    @Autowired private val jwtTokenService: JwtTokenService
 ) : JpaTestBase() {
 
     @Nested
@@ -20,46 +26,46 @@ internal class ConfigurationControllerTest(
 
         private val requestPath = Path("/api/configuration")
 
-        //TODO 1.1 release workaround:
+        @Test
+        fun `Given unauthorized request, should return anonymous configuration`() {
+            mockMvc.get(path = requestPath) {
+                isSuccess()
+                responseJsonPath("$.maxTemporaryFileSize")
+                    .equalsValue(ConfigurationKey.ANONYMOUS_MAX_FILE_SIZE.defaultValue)
+                responseJsonPath("$.maxFileLifetime")
+                    .equalsValue(ConfigurationKey.ANONYMOUS_MAX_FILE_LIFETIME.defaultValue)
+                responseJsonPath("$.defaultFileLifetime")
+                    .equalsValue(ConfigurationKey.ANONYMOUS_DEFAULT_FILE_LIFETIME.defaultValue)
+                responseJsonPath("$.permanentAllowed")
+                    .equalsValue(false)
+                responseJsonPath("$.maxPermanentFileSize")
+                    .equalsValue(0)
+            }
+        }
 
-//        @Test
-//        fun `Given unauthorized request, should return anonymous configuration`() {
-//            mockMvc.get(path = requestPath) {
-//                isSuccess()
-//                responseJsonPath("$.maxTemporaryFileSize")
-//                        .equalsValue(ConfigurationKey.ANONYMOUS_MAX_FILE_SIZE.defaultValue)
-//                responseJsonPath("$.maxFileLifetime")
-//                        .equalsValue(ConfigurationKey.ANONYMOUS_MAX_FILE_LIFETIME.defaultValue)
-//                responseJsonPath("$.defaultFileLifetime")
-//                        .equalsValue(ConfigurationKey.ANONYMOUS_DEFAULT_FILE_LIFETIME.defaultValue)
-//                responseJsonPath("$.permanentAllowed")
-//                        .equalsValue(false)
-//                responseJsonPath("$.maxPermanentFileSize")
-//                        .equalsValue(0)
-//            }
-//        }
-//
-//        @Test
-//        @DocumentResponse
-//        fun `Given authorized request, should return user-specific details`() {
-//            val user = requireNotNull(userService.findUser(Username("root")))
-//            val refreshToken = jwtTokenService.issueRefreshToken(user)
-//            val accessToken = jwtTokenService.issueAccessToken(refreshToken)
-//
-//            mockMvc.get(path = requestPath,
-//            headers = HttpHeaders().authorization(accessToken)) {
-//                isSuccess()
-//                responseJsonPath("$.maxTemporaryFileSize")
-//                        .equalsValue(ConfigurationKey.DEFAULT_USER_MAX_TEMPORARY_FILE_SIZE.defaultValue)
-//                responseJsonPath("$.maxFileLifetime")
-//                        .equalsValue(ConfigurationKey.DEFAULT_USER_MAX_FILE_LIFETIME.defaultValue)
-//                responseJsonPath("$.defaultFileLifetime")
-//                        .equalsValue(ConfigurationKey.DEFAULT_USER_DEFAULT_FILE_LIFETIME.defaultValue)
-//                responseJsonPath("$.permanentAllowed")
-//                        .equalsValue(true)
-//                responseJsonPath("$.maxPermanentFileSize")
-//                        .equalsValue(ConfigurationKey.DEFAULT_USER_MAX_PERMANENT_FILE_SIZE.defaultValue)
-//            }
-//        }
+        @Test
+        @DocumentResponse
+        fun `Given authorized request, should return user-specific details`() {
+            val user = requireNotNull(userService.findUser(Username("root")))
+            val refreshToken = jwtTokenService.issueRefreshToken(user)
+            val accessToken = jwtTokenService.issueAccessToken(refreshToken)
+
+            mockMvc.get(
+                path = requestPath,
+                headers = HttpHeaders().authorization(accessToken)
+            ) {
+                isSuccess()
+                responseJsonPath("$.maxTemporaryFileSize")
+                    .equalsValue(ConfigurationKey.DEFAULT_USER_MAX_TEMPORARY_FILE_SIZE.defaultValue)
+                responseJsonPath("$.maxFileLifetime")
+                    .equalsValue(ConfigurationKey.DEFAULT_USER_MAX_FILE_LIFETIME.defaultValue)
+                responseJsonPath("$.defaultFileLifetime")
+                    .equalsValue(ConfigurationKey.DEFAULT_USER_DEFAULT_FILE_LIFETIME.defaultValue)
+                responseJsonPath("$.permanentAllowed")
+                    .equalsValue(true)
+                responseJsonPath("$.maxPermanentFileSize")
+                    .equalsValue(ConfigurationKey.DEFAULT_USER_MAX_PERMANENT_FILE_SIZE.defaultValue)
+            }
+        }
     }
 }
