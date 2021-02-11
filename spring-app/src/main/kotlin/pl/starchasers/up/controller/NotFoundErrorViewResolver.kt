88127bba60ce0845789d295ccfb9855de7ef31ab
Hubert Balcerzak <hubertbalc@gmail.com>
2021-02-11 03:22:34 +0100

    add API error handling

--- spring-app/src/main/kotlin/pl/starchasers/up/controller/NotFoundErrorViewResolver.kt
+++ spring-app/src/main/kotlin/pl/starchasers/up/controller/NotFoundErrorViewResolver.kt
@@ -4,16 +4,20 @@ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolve
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.ModelAndView
+import pl.starchasers.up.exception.NotFoundException
 import javax.servlet.http.HttpServletRequest
 
 @Component
-class NotFoundErrorViewResolver : ErrorViewResolver{
+class NotFoundErrorViewResolver : ErrorViewResolver {
 
     override fun resolveErrorView(
-        request: HttpServletRequest?,
-        status: HttpStatus?,
-        model: MutableMap<String, Any>?
+        request: HttpServletRequest,
+        status: HttpStatus,
+        model: MutableMap<String, Any>
     ): ModelAndView {
+        val path = model["path"]
+        if (path is String && path.startsWith("/api/")) throw NotFoundException()
+
         return ModelAndView("forward:/index.html")
     }
 }
