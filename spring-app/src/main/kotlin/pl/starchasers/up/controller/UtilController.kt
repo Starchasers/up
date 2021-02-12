2d447535469fffdf96bb124f94d0194b13d5435e
Hubert Balcerzak <hubertbalc@gmail.com>
2021-02-12 19:58:33 +0100

    fix lint

--- spring-app/src/main/kotlin/pl/starchasers/up/controller/UtilController.kt
+++ spring-app/src/main/kotlin/pl/starchasers/up/controller/UtilController.kt
@@ -3,7 +3,6 @@ package pl.starchasers.up.controller
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.ModelAndView
 
 @RestController
 class UtilController {
@@ -24,5 +23,5 @@ class UtilController {
           "FileFormName": "file",
           "URL": "$domain/u/${'$'}json:key${'$'}"
         }
-    """.trimIndent()
+        """.trimIndent()
 }
