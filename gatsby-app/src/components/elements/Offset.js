3cd392e5bccd079973c4ce76deebdf7145615ce2
Kuboczoch <jakub.pokorski@protonmail.com>
2021-02-28 02:38:50 +0100

    :art: Added filter brightness

--- gatsby-app/src/components/elements/Offset.js
+++ gatsby-app/src/components/elements/Offset.js
@@ -1,5 +1,9 @@
 import styled, { css } from 'styled-components'
 
+const dragActive = css`
+  filter: brightness(50%);
+`
+
 const Offset = styled('div')`
   padding-top: 17vh;
   min-height: 100vh;
@@ -8,6 +12,8 @@ const Offset = styled('div')`
   &:focus {
     outline: unset;
   }
+  
+  ${props => props.dragActive && dragActive}
 `
 
 export default Offset
