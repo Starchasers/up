77e117057bbaca4dc09936883dcd0cfc344ea10c
Jakub Pokorski <jakub.pokorski@protonmail.com>
2021-03-05 11:20:51 +0100

    :pencil2: Update drop message
    Co-authored-by: Hubert Balcerzak <hubertbalc@gmail.com>

--- gatsby-app/src/providers/file-upload-provider.js
+++ gatsby-app/src/providers/file-upload-provider.js
@@ -138,7 +138,7 @@ const FileUploadProvider = ({ children }) => {
             <GlobalDropZone active={dragActive} {...getRootProps()}>
               <GlobalDropZone.Container>
                 <GlobalDropZone.Text>
-                  Drop file here!
+                  Drop file here
                 </GlobalDropZone.Text>
               </GlobalDropZone.Container>
             </GlobalDropZone>
